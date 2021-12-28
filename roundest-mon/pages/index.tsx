21b2827ec5f1a9dc90a848248b83f4681123e41e
Reinald Reynoso <reireynoso@gmail.com>
2021-12-27 19:01:49 -0500

    UI setup

--- roundest-mon/pages/index.tsx
+++ roundest-mon/pages/index.tsx
@@ -4,8 +4,14 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className=''>
-      Hello World
+    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='text-2xl text-center'>Which Pokemon is Rounder?</div>
+      <div className='p-2'/>
+      <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
+        <div className='w-16 h-16 bg-red-200'></div>
+        <div className='p-8'>Vs</div>
+        <div className='w-16 h-16 bg-red-200'></div>
+      </div>
     </div>
   ) 
 }
