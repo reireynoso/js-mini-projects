4a1c77237b9c0c01a4ba860c6c87c2cc1373b813
Reinald Reynoso <reireynoso@gmail.com>
2021-12-27 20:02:27 -0500

    File restructure and add trpc

--- roundest-mon/src/pages/index.tsx
+++ roundest-mon/src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { trpc } from '../utils/trpc'
 
 const Home: NextPage = () => {
+  const {data, isLoading} = trpc.useQuery(["hello", {text: "Rei"}]);
+
+  if(isLoading) return <div>Loading...</div>
+
+  if(data) return <div>{data.greeting}</div>
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className='text-2xl text-center'>Which Pokemon is Rounder?</div>
