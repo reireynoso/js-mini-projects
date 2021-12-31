9b8f0da9eaf6eadae14cad2322c0758988b70bae
Reinald Reynoso <reireynoso@gmail.com>
2021-12-31 14:37:34 -0500

    Added loaders and button

--- remix-blog/app/routes/posts/index.jsx
+++ remix-blog/app/routes/posts/index.jsx
@@ -1,7 +1,48 @@
+import {Link, useLoaderData} from 'remix';
+
+export const loader = () => {
+    const data = {
+        posts: [
+            {
+                id: 1,
+                title: 'Post 1',
+                body: "This is a test post 1"
+            },
+            {
+                id: 2,
+                title: 'Post 2',
+                body: "This is a test post 2"
+            },
+            {
+                id: 2,
+                title: 'Post 2',
+                body: "This is a test post 2"
+            }
+        ]
+    }
+    return data;
+}
 function PostItems() {
+    const {posts} = useLoaderData();
     return (
         <div>
-            <h1>Posts</h1>
+            <div className='page-header'>
+                <h1>Posts</h1>
+                <Link to="/posts/new" className='btn'>
+                    New Post
+                </Link>  
+            </div>
+            <ul className='posts-list'>
+                {
+                    posts.map(post => (
+                        <li key={post.id}>
+                            <Link to={post.id}>
+                                <h3>{post.title}</h3>
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
