59f1d4ca4de11c82e2709da5b38cee54b5e12e92
Reinald Reynoso <reireynoso@gmail.com>
2021-12-30 21:04:36 -0500

    Add percent round count to results page

--- roundest-mon/src/pages/results.tsx
+++ roundest-mon/src/pages/results.tsx
@@ -4,15 +4,25 @@ import { prisma } from "../backend/utils/prisma";
 import Image from 'next/image';
 type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
 
-const PokemonListing:React.FC<{pokemon: PokemonQueryResult[number]}> = (props) => {
-    return <div className="flex border-b p-2 items-center">
-        <Image
-            src={props.pokemon.spriteUrl}
-            width={32}
-            height={32}
-            layout="fixed"
-        />
-        <div className="capitalize">{props.pokemon.name}</div>
+const generateCountPercent = (pokemon: PokemonQueryResult[number]) => {
+    const {VoteFor, VoteAgainst} = pokemon._count;
+    if(VoteFor + VoteAgainst == 0){
+        return 0;
+    }
+    return (VoteFor / (VoteFor + VoteAgainst)) * 100;
+}
+const PokemonListing:React.FC<{pokemon: PokemonQueryResult[number]}> = ({pokemon}) => {
+    return <div className="flex border-b p-2 items-center justify-between">
+        <div className="flex">
+            <Image
+                src={pokemon.spriteUrl}
+                width={32}
+                height={32}
+                layout="fixed"
+            />
+            <div className="capitalize">{pokemon.name}</div>
+        </div>
+        <div className="pr-4">{generateCountPercent(pokemon)} %</div>
     </div>
 }
 const ResultsPage:React.FC<{pokemon: PokemonQueryResult}> = (props) => {
