f473d9ed366384d208ef4fcab9c88116673089d1
Reinald Reynoso <reireynoso@gmail.com>
2022-01-01 16:25:37 -0500

    Server side validation for post

--- remix-blog/app/routes/posts/new.jsx
+++ remix-blog/app/routes/posts/new.jsx
@@ -1,5 +1,17 @@
-import { Link, redirect } from "remix";
+import { Link, redirect, useActionData, json } from "remix";
 import { db } from "~/utils/db.server";
+
+function validateTitle(title){
+    if(typeof title !== 'string' || title.length < 3){
+        return 'Title should be at least 3 characters long';
+    }
+}
+
+function validateBody(body){
+    if(typeof body !== 'string' || body.length < 3){
+        return 'Body should be at least 3 characters long';
+    }
+}
 
 export const action = async({request}) => {
     const form = await request.formData();
@@ -11,6 +23,21 @@ export const action = async({request}) => {
         body
     };
 
+    const fieldErrors = {
+        title: validateTitle(title),
+        body: validateBody(body)
+    }
+
+    if(Object.values(fieldErrors).some(Boolean)){
+        console.log(fieldErrors);
+        return json({
+            fieldErrors,
+            fields
+        }, {
+            data:fields
+        })
+    }
+
     const post = await db.post.create({
         data: fields
     });
@@ -19,6 +46,9 @@ export const action = async({request}) => {
 }
 
 function NewPost() {
+    const actionData = useActionData();
+
+
     return (
         <>
             <div className="page-header">
@@ -32,11 +62,26 @@ function NewPost() {
                 <form method="POST">
                     <div className="form-control">
                         <label htmlFor="title">Title</label>
-                        <input type="text" name="title" id="title"/>
+                        <input 
+                            type="text" 
+                            name="title" 
+                            id="title"
+                            defaultValue={actionData?.fields?.title}
+                            />
+                        <div className="error">
+                            <p>{actionData?.fieldErrors.title && (actionData.fieldErrors.title)}</p>
+                        </div>
                     </div>
                     <div className="form-control">
                         <label htmlFor="body">Post Body</label>
-                        <textarea name="body" id="body"/>
+                        <textarea 
+                            name="body" 
+                            id="body"
+                            defaultValue={actionData?.fields?.body}
+                            />
+                        <div className="error">
+                            <p>{actionData?.fieldErrors.body && (actionData.fieldErrors.body)}</p>
+                        </div>
                     </div>
                     <button type="submit" className="btn btn-block">
                         Add Post
